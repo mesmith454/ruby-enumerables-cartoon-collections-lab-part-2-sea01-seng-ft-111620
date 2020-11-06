48a58718f5d44d4142739e6207fcb30628b5e63a
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-06 21:14:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,4 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find { |call| call == valid_calls.all?
   }
-  puts call
 end
